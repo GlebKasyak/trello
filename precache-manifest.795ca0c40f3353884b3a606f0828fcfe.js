f7a84392f0c0c2de98f13c9d9308cb2fc4179630
Gleb <gkebkasyak@gmail.com>
2019-12-20 00:04:24 +0300

    Updates

---
+++ precache-manifest.795ca0c40f3353884b3a606f0828fcfe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c888827d90816261eba99868afdc0ea",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "404ed31cf83eb6027bd4",
+    "url": "/trello/static/css/main.75bcc587.chunk.css"
+  },
+  {
+    "revision": "7a5e4ab1473ad092af77",
+    "url": "/trello/static/js/2.703d4222.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trello/static/js/2.703d4222.chunk.js.LICENSE"
+  },
+  {
+    "revision": "404ed31cf83eb6027bd4",
+    "url": "/trello/static/js/main.5be53d89.chunk.js"
+  },
+  {
+    "revision": "2ba4bebbe67e27faf1fc",
+    "url": "/trello/static/js/runtime-main.8f5d4f17.js"
+  },
+  {
+    "revision": "b4e7124aebe83563440fc77620364520",
+    "url": "/trello/static/media/add.b4e7124a.svg"
+  },
+  {
+    "revision": "7bce26d56ccd2ae59725f43eccf3ac8b",
+    "url": "/trello/static/media/background.7bce26d5.jpg"
+  },
+  {
+    "revision": "e82c0deb46d38f804bbe1180dc68b2a3",
+    "url": "/trello/static/media/clear.e82c0deb.svg"
+  }
+]);
